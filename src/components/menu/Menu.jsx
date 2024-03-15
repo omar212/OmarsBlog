0e9c100e3d4458d1e90d314d5a7c0e0a7feeb07f
Omar Elnagdy <omarelnagdy16@gmail.com>
2024-03-15 07:37:33 -0400

    fixed bug in menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./menu.module.css";
-import Link from "next/link";
 import MenuPosts from "@/components/menuPosts/MenuPosts";
 import MenuCategories from "@/components/menuCategories/MenuCategories";
 const Menu = () => {
@@ -9,17 +8,17 @@ const Menu = () => {
 
             {/* Without Image */}
             <h2 className={styles.subtitle}>What's hot</h2>
-            <h1 className={styles.title }>Most Popular</h1>
+            <h1 className={styles.title}>Most Popular</h1>
             <MenuPosts withImage={false}/>
 
             {/* Categories */}
             <h2 className={styles.subtitle}>Discover by topic</h2>
-            <h1 className={styles.title }>Categories</h1>
+            <h1 className={styles.title}>Categories</h1>
             <MenuCategories />
 
             {/* With Image */}
             <h2 className={styles.subtitle}>Chosen by the editor</h2>
-            <h1 className={styles.title }>Editors Pick</h1>
+            <h1 className={styles.title}>Editors Pick</h1>
             <MenuPosts withImage />
         </div>
     )
