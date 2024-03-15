fffcc98d3f39df1b582fa3a5b720d5e1a754b564
Omar Elnagdy <omarelnagdy16@gmail.com>
2024-03-15 08:19:36 -0400

    fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,8 @@
 import Featured from "@/components/featured/Featured";
 import CategoryList from "@/components/categoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
-import Pagination from "@/components/pagination/Pagination";
 import Menu from "@/components/menu/Menu";
 import styles from "./homepage.module.css";
-import Link from "next/link";
-
 
 export default function Home({ searchParams }) {
 
